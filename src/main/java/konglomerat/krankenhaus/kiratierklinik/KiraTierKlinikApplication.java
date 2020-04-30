bca40c2cdfad00e622871ab18fac3cf93ca0e7b2
kirishanthchethurajah <kchethur@uni-bremen.de>
2020-04-30 19:02:42 +0200

    Bare Spring Boot Commit

---
+++ src/main/java/konglomerat/krankenhaus/kiratierklinik/KiraTierKlinikApplication.java
@@ -0,0 +1,13 @@
+package konglomerat.krankenhaus.kiratierklinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiraTierKlinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiraTierKlinikApplication.class, args);
+    }
+
+}
